c3b2a39764afba1d125527f561b601eee7bb84b3
womendexinshijie <womendexinshijie@sina.com>
2016-04-08 13:44:07 +0800

    Initial Commit

---
+++ QQ/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QQ
+//
+//  Created by junhaoshen on 16/4/8.
+//  Copyright © 2016年 junhaoshen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
